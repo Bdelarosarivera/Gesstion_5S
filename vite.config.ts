af53debddb073d04b0b32baba413a9d13bb36774
Bartolo de la Rosa Rivera <157636952+Bdelarosarivera@users.noreply.github.com>
2025-12-16 09:51:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
-    return {
+    return { base: '/Gestion_5S/', // 👈 MUY IMPORTANTE
       server: {
         port: 3000,
         host: '0.0.0.0',
